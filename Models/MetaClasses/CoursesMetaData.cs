feffa85732e410dda282972f91572c1cee5dd2c7
Aman Agrawal <aman9893089064@gmail.com>
2022-02-28 17:00:11 +0530

    Finally Enrollments data set issue fixed. Courses and Student page view modified

---
+++ Models/MetaClasses/CoursesMetaData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.Models
+{
+    public class CoursesMetaData
+    {
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Range(1,8)]
+        public int Credits { get; set; }
+    }
+
+    [MetadataType(typeof(CoursesMetaData))]
+    public partial class Course
+    {
+
+    }
+}
