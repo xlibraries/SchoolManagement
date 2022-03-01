6c0e44b95c07997ccd26eec3bd45b8540d7b4497
Aman Agrawal <aman9893089064@gmail.com>
2022-03-01 16:57:23 +0530

    Added User data point

--- Models/Student.cs
+++ Models/Student.cs
@@ -25,6 +25,7 @@ namespace SchoolManagement.Models
         public string FirstName { get; set; }
         public Nullable<System.DateTime> EnrollmentDate { get; set; }
         public string MiddleName { get; set; }
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Enrollement> Enrollements { get; set; }
